1a785b37c05655b50e2ede8fe915fb34294dec9d
Gabriel Gevert <gevertlolz@gmail.com>
2025-01-07 20:10:07 -0300

    update github pages

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
