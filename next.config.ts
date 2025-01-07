eff302730e3ec56bd1dc87b7710c5ba30291f780
Gabriel Gevert <gevertlolz@gmail.com>
2025-01-07 20:06:46 -0300

    upload of github pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  trailingSlash: true,
   compiler: {
     styledComponents: true,
   },
