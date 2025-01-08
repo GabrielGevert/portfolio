95fb98da1c1ad0fb7bd4a160ba8a16efbe9dfb73
Gabriel Gevert <gevertlolz@gmail.com>
2025-01-07 22:42:42 -0300

    update root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Header } from "@components";
 import "@/styles/global.css";
 import { LanguageProvider } from "@/utils/LanguageContext";
+import StyledComponentsRegistry from "@/utils/registry";
 
 export const metadata: Metadata = {
   title: "Gabriel Gevert - Portfolio",
@@ -16,10 +17,12 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
-        <LanguageProvider>
-          <Header />
-          <main>{children}</main>
-        </LanguageProvider>
+        <StyledComponentsRegistry>
+          <LanguageProvider>
+            <Header />
+            <main>{children}</main>
+          </LanguageProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,6 @@ import Opening from "@/sections/opening";
 import Projects from "@/sections/projects";
 import Stacks from "@/sections/stacks";
 
-// import Faq from "@/sections/Home-Page/FaqSection";
-// import JobsList from "@/sections/Home-Page/JobsSection";
-// import Newsletter from "@/sections/Home-Page/Newsletter";
-// import Testimonials from "@/sections/Home-Page/Testimonials";
-// import WhyUs from "@/sections/Home-Page/WhyUsSection";
-// import { Banner } from "@components";
-
 export default function Home() {
   return (
     <>
